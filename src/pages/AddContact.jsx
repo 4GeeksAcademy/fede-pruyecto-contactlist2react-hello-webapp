5ad5f0c5899caafa5cee355266fbfd582f5874d8
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-04-21 19:56:13 +0000

    se ha resuelto la funcion de editar con el mismo componente que crear

--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -1,33 +1,33 @@
 import { Link, useParams } from "react-router-dom"
 import { useEffect, useState, useReducer } from "react";
-import { addContact as createContact } from "../functions";
+import { addContact as createContact, editContact as editTheContact } from "../functions";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 
-export const AddContact = ()=> {
-    const {state, dispatch} = useGlobalReducer();
+export const AddContact = () => {
+    const { state, dispatch } = useGlobalReducer();
     const [formData, setFormData] = useState({
         name: "",
         phone: "",
         email: "",
         address: "",
-      });
-    const {id} = useParams();
+    });
+    const { id } = useParams();
     console.log(formData);
 
     useEffect(() => {
         if (id) {
-            const currrentContact= state.contacts.find(contact => contact.id == id)
+            const currrentContact = state.contacts.find(contact => contact.id == id)
             setFormData(currrentContact)
         }
     }, [])
 
 
     const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
     };
 
-    const handleSubmit = async (e)=> {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const newContact = {
             name: formData.name,
@@ -45,35 +45,69 @@ export const AddContact = ()=> {
                 phone: "",
                 email: "",
                 address: "",
-              });
+            });
         } else {
             console.error("Error al agregar el contacto.")
         }
     };
-    
-    return(
+
+    const handleEdit = async (e) => {
+        e.preventDefault();
+        if (!id) {
+            console.error("No se puede editar sin un ID de contacto.");
+            return;
+        }
+        const editContactData = {
+            name: formData.name,
+            phone: formData.phone,
+            email: formData.email,
+            address: formData.address,
+        };
+
+        const success = await editTheContact(id, editContactData); 
+
+        if (success) {
+            console.log("Contacto editado exitosamente:", success);
+            dispatch({ type: "update_contact", payload: { id: id, updatedContact: editContactData } });
+            setFormData({
+                name: "",
+                phone: "",
+                email: "",
+                address: "",
+            });
+        } else {
+            console.error("Error al editar el contacto.");
+            
+        }
+    };
+
+    return (
         <div>
-            <h1 className="text-center"></h1>
+            <h1 className="text-center">{id ? 'Editar Contacto' : 'Agregar Nuevo Contacto'}</h1>
             <form className="p-5">
                 <div className="mb-3">
                     <label className="form-label">Name</label>
-                    <input type="text" name="name" className="form-control" onChange={handleChange} value={formData.name}/>
+                    <input type="text" name="name" className="form-control" onChange={handleChange} value={formData.name} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Phone</label>
-                    <input type="text" name="phone" className="form-control" onChange={handleChange} value={formData.phone}/>
+                    <input type="text" name="phone" className="form-control" onChange={handleChange} value={formData.phone} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">email</label>
-                    <input type="email" name="email" className="form-control" onChange={handleChange} value={formData.email}/>
+                    <input type="email" name="email" className="form-control" onChange={handleChange} value={formData.email} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Address</label>
-                    <input type="text" name="address" className="form-control" onChange={handleChange} value={formData.address}/>
+                    <input type="text" name="address" className="form-control" onChange={handleChange} value={formData.address} />
                 </div>
             </form>
             <div className="d-grid gap-2 col-6 mx-auto">
-                <button className="btn btn-success" type="button" onClick={handleSubmit}>Add contact</button>
+                {!id ? (
+                    <button className="btn btn-success" type="button" onClick={handleSubmit}>Add contact</button>
+                ) : (
+                    <button className="btn btn-primary" type="button" onClick={handleEdit}>Save Changes</button>
+                )}
             </div>
             <Link to="/contactslist">
                 <p className="mx-5">Ir a lista de contactos</p>
--- src/pages/ContactsList.jsx
+++ src/pages/ContactsList.jsx
@@ -6,7 +6,7 @@ import useGlobalReducer from "../hooks/useGlobalReducer";
 
 
 export const ContactsList = () => {
-    const {state, dispatch} = useGlobalReducer();
+    const { state, dispatch } = useGlobalReducer();
     const { contacts } = state;
 
     const loadInitialContacts = async () => {
@@ -23,7 +23,7 @@ export const ContactsList = () => {
             console.log(`Contacto con ID ${id} eliminado.`);
         } else {
             console.error(`No se pudo eliminar el contacto con ID ${id}.`);
-            // Aquí podrías mostrar un mensaje de error al usuario
+            
         }
     };
 
@@ -32,42 +32,39 @@ export const ContactsList = () => {
     }, [])
 
     return (
-        <div className="">
-            <Link to="/addcontact">
-                <button className="btn btn-success">Add New Contact</button>
-            </Link>
-            <div className="d-flex m-1">
-                <img className="imgUser" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                <ul>
-                    <li>nombre:</li>
-                    <li>telefono</li>
-                    <li>direccion</li>
-                    <li>email</li>
-                </ul>
-                <button>editar</button>
-                <button>eliminar</button>
+        <div className="containerContact">
+            <div>
+                <Link to="/addcontact" className="d-flex justify-content-end m-5">
+                    <button className="btn btn-success ">Add New Contact</button>
+                </Link>
             </div>
-            <div className="">
+            <div className="d-flex flex-column ">
                 {contacts.map((contact) => (
                     <div key={contact.id} className="d-flex m-1">
-                        <img className="imgUser" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <ul>
-                            <li>nombre: {contact.name}</li>
-                            <li>telefono: {contact.phone}</li>
-                            <li>direccion: {contact.address}</li>
-                            <li>email: {contact.email}</li>
-                        </ul>
-                        <Link to={`/contactedit/${contact.id}`}>
-                            <button className="btn btn-success">Editar</button>
-                        </Link>
-                        <div>
-                            <button className="btn btn-danger" onClick={() => handleDeleteContact(contact.id)}>eliminar</button>
+                        <div className="d-flex">
+                            <img className="imgUser" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                            <ul>
+                                <li>nombre: {contact.name}</li>
+                                <li>telefono: {contact.phone}</li>
+                                <li>direccion: {contact.address}</li>
+                                <li>email: {contact.email}</li>
+                            </ul>
                         </div>
+                        <div className="d-flex ms-auto">
 
+                            <Link to={`/contactedit/${contact.id}`}>
+                                <button className="btn btn-success">Editar</button>
+                            </Link>
+                            <div>
+                                <button className="btn btn-danger" onClick={() => handleDeleteContact(contact.id)}>eliminar</button>
+                            </div>
+
+                        </div>
                     </div>
                 ))}
                 {contacts.length === 0 && <p>No hay contactos para mostrar.</p>}
             </div>
+
         </div>
     )
 }
